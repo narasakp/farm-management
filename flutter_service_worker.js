e1b4fdc3ddb8b841513c366b5552c832165c34f0
nara <narasakp@gmail.com>
2025-08-28 15:52:07 +0700

    Deploy Flutter web app to GitHub Pages - fresh build with all assets

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "d24aa5ba0abc25d2e4ded4d389a90165",
+"flutter_bootstrap.js": "ab8054571320f8aa1cdf5ea0e50c927b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "index.html": "5d4c9e2cdda3b1e632ebf1970b3c3103",
 "/": "5d4c9e2cdda3b1e632ebf1970b3c3103",
 "main.dart.js": "95a8cd354b8b53e7c565ff5306f55c82",
-"manifest.json": "b9061298e43ecde523a625c075f9b00c",
+"manifest.json": "c105db012daed52702ab7c677b069f4f",
 "version.json": "94b99345aad596aac2121cb9a9f387a6"};
 // The application shell files that are downloaded before a service worker can
 // start.
