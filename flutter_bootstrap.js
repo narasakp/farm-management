325eaf9af891f155219be2768e3ea37b9cf24ca9
nara <narasakp@gmail.com>
2025-08-28 19:07:40 +0700

    Force rebuild with version bump - New features should now work

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1377096908"
+    serviceWorkerVersion: "3118376989"
   }
 });
